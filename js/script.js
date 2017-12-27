3fde30f3adb4020d29b71377e936776e0f4f852e
Shawn R. D. Moran <shawn.r.d.moran@gmail.com>
2017-12-27 10:12:20 -0700

    Add code to load Article Search API

--- js/script.js
+++ js/script.js
@@ -21,6 +21,18 @@ function loadData() {
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
+    //NYTimes AJAX request
+    var nytimesUrl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr +    '&sort=newest&api-key=7c89ae1b6e3b4e58b71d39428d8b59a7";
+    $.getJSON("nytimesUrl", function (data) {
+      $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+
+      articles = data.response.docs;
+      for (var i = 0; i < articles.length; i++) {
+        var article = articles[i];
+        $nytElem.append('<li class="article">' + '<a href="'+ article.web_url +'">'+ article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + '</li>');
+      }
+    })
+
     return false;
 };
 
