8e574f4947527b2e460490f5d20fc0e3c4fe1759
Shawn R. D. Moran <shawn.r.d.moran@gmail.com>
2017-12-27 10:17:05 -0700

    Update code for Article Search API

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ function loadData() {
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
     //NYTimes AJAX request
-    var nytimesUrl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr +    '&sort=newest&api-key=7c89ae1b6e3b4e58b71d39428d8b59a7"
+    var nytimesUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr +    '&sort=newest&api-key=7c89ae1b6e3b4e58b71d39428d8b59a7"
     $.getJSON("nytimesUrl", function (data) {
       $nytHeaderElem.text('New York Times Articles About ' + cityStr);
 
